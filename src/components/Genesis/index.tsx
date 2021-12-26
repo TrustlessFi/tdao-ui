b28a5cd4d7422e7e6067374ec9318929b6e72021
iwantausernamewithoutnumbers <iwantausernamewithoutnumbers@protonmail.com>
2021-12-26 12:46:56 -0800

    * Refactor/streamline Genesis page

--- src/components/Genesis/index.tsx
+++ src/components/Genesis/index.tsx
@@ -1,10 +1,10 @@
-import React from "react"
-import * as ethers from "ethers";
-import { Button } from "carbon-components-react"
+import React, { useState } from "react"
+import * as ethers from "ethers"
+import { Button, TextInput, Form, FormGroup } from "carbon-components-react"
 
 import AppTile from "../library/AppTile"
 import SimpleTable from "../library/SimpleTable"
-import { unscale } from '../../utils';
+import { unscale } from "../../utils"
 
 import { useAppSelector as selector, useAppDispatch } from "../../app/hooks"
 import {
@@ -15,37 +15,56 @@ import { waitForGenesisClaimAllocations } from "../../slices/genesis"
 
 const Genesis: React.FunctionComponent = () => {
   const dispatch = useAppDispatch()
-  const downloadRef = React.createRef<HTMLAnchorElement>();
+  const downloadRef = React.createRef<HTMLAnchorElement>()
 
   const chainID = selector((state) => state.chainID.chainID)
-  const userAddress = selector((state) => state.wallet.address);
+  const userAddress = selector((state) => state.wallet.address)
   // process debt and liquidity
   const { debt, liquidity } = waitForGenesisPositions(selector, dispatch) || {
     debt: [],
     liquidity: [],
   }
   // get debt owners
-  const debtOwnersSet = new Set(debt.map((pos) => pos.owner));
-  const debtOwners = Array.from(debtOwnersSet);
+  const debtOwnersSet = new Set(debt.map((pos) => pos.owner))
+  const debtOwners = Array.from(debtOwnersSet)
   // get liquidity owners who also have debt
-  const liquidityOwnersSet = new Set(liquidity.map((pos) => pos.owner));
-  const liquidityOwners = Array.from(liquidityOwnersSet).filter((liquidityOwner)=>debtOwnersSet.has(liquidityOwner));
+  const liquidityOwnersSet = new Set(liquidity.map((pos) => pos.owner))
+  const liquidityOwners = Array.from(liquidityOwnersSet).filter(
+    (liquidityOwner) => debtOwnersSet.has(liquidityOwner)
+  )
 
   // process genesis allocations
   const allAllocations = waitForGenesisAllocations(selector, dispatch) || {}
   const allocations = userAddress ? allAllocations[userAddress] || [] : []
   const genesisAllocation = selector((state) => state.chainID.genesisAllocation)
 
+  // eligibility search box
+  const [addressFilter, setAddressFilter] = useState("")
+
   // view data
   //tables
-  const liquidityRows = liquidityOwners.map((address: string) => ({
-    key: address,
-    data: {"Address": address}
-  }));
-  const debtRows = debtOwners.map((address: string) => ({
-    key: address,
-    data: {"Address": address}
-  }));
+  type Eligibility = { liquidity: boolean; debt: boolean }
+  const eligibleOwners = {} as { [key: string]: Eligibility }
+  debtOwners.map(
+    (address) => (eligibleOwners[address] = { liquidity: false, debt: true })
+  )
+  liquidityOwners.map((address) => (eligibleOwners[address].liquidity = true))
+  const eligibilityRows = Object.entries(eligibleOwners)
+    .map(([address, { liquidity, debt }]) => {
+      const booleanIcon = (value: boolean) => (value ? "✔" : "✖")
+      return {
+        key: address,
+        data: {
+          Address: address,
+          Debt: booleanIcon(debt),
+          Liquidity: booleanIcon(liquidity),
+        },
+      }
+    })
+    .filter(
+      (data) => addressFilter === "" || data.key.indexOf(addressFilter) > -1
+    )
+    .sort((a, b) => a.key.localeCompare(b.key))
   const allocationRows = allocations.map(({ roundID, count }) => ({
     key: `${roundID}-${count}`,
     data: {
@@ -55,71 +74,99 @@ const Genesis: React.FunctionComponent = () => {
   }))
 
   //download url
-  const downloadDisabled = (genesisAllocation === null || (debt.length === 0 && liquidity.length === 0) || !chainID)
+  const downloadDisabled =
+    genesisAllocation === null ||
+    (debt.length === 0 && liquidity.length === 0) ||
+    !chainID
   let downloadAnchorProps = {
-    style: {textDecoration: "none", color: "inherit"},
+    style: { textDecoration: "none", color: "inherit" },
     ref: downloadRef,
-    hidden: true
-  } as React.HTMLProps<HTMLAnchorElement>;
-  if(!downloadDisabled) {
+    hidden: true,
+  } as React.HTMLProps<HTMLAnchorElement>
+  if (!downloadDisabled) {
     const data = {
-      chainID, genesisAllocationAddress: genesisAllocation, liquidityPositions: liquidityOwners, debtPositions: debtOwners
-    };
-    const blob = new Blob([JSON.stringify(data)], {type: 'text/json'});
+      chainID,
+      genesisAllocationAddress: genesisAllocation,
+      liquidityPositions: liquidityOwners,
+      debtPositions: debtOwners,
+    }
+    const blob = new Blob([JSON.stringify(data)], { type: "text/json" })
     downloadAnchorProps = {
       ...downloadAnchorProps,
       href: URL.createObjectURL(blob),
-      download: `genesis-${_dateString(new Date())}-${chainID}.json`
+      download: `genesis-${_dateString(new Date())}-${chainID}.json`,
     }
   }
   const downloadClick = (event: React.MouseEvent) => {
     const hiddenAnchorElement = downloadRef.current
-    if(!hiddenAnchorElement) return
-    if(downloadDisabled) return
-    hiddenAnchorElement.click();
+    if (!hiddenAnchorElement) return
+    if (downloadDisabled) return
+    hiddenAnchorElement.click()
   }
 
   // claim url
-  const claimDisabled = (genesisAllocation === null || allocations.length === 0)
+  const claimDisabled = genesisAllocation === null || allocations.length === 0
   const claimClick = () => {
-    if(claimDisabled) return
-    if(claimDisabled) return
-    if(genesisAllocation === null) return
+    if (claimDisabled) return
+    if (claimDisabled) return
+    if (genesisAllocation === null) return
     dispatch(waitForGenesisClaimAllocations({ genesisAllocation, allocations }))
   }
 
   return (
     <>
       <AppTile title="Genesis" className="genesis">
-          <Button kind={"secondary"} disabled={downloadDisabled} onClick={downloadClick}>
-            Download Genesis Data
-          </Button>
-          <Button kind={"primary"} onClick={claimClick} disabled={claimDisabled}>
-            Claim Tokens
-          </Button>
-          <a {...downloadAnchorProps}>Hidden Genesis Data Download Link</a>
+        <div style={{ display: "flex", marginBottom: "5px" }}>
+          <div style={{ flex: 1 }}>
+            <TextInput
+              id={"address-search"}
+              hideLabel={true}
+              labelText={"Search"}
+              placeholder={"Enter address"}
+              value={addressFilter}
+              onChange={(event) => setAddressFilter(event.target.value)}
+              light={true}
+              size={"sm"}
+            />
+          </div>
+          <div>
+            <Button
+              kind={"primary"}
+              onClick={claimClick}
+              disabled={claimDisabled}
+              size={"sm"}
+            >
+              Claim
+            </Button>
+            <Button
+              kind={"secondary"}
+              disabled={downloadDisabled}
+              onClick={downloadClick}
+              size={"sm"}
+            >
+              Save
+            </Button>
+          </div>
+        </div>
+        <SimpleTable rows={eligibilityRows} />
       </AppTile>
-      <AppTile title={"Debt"} className={"debt"}>
-        <SimpleTable rows={debtRows} />
-      </AppTile>
-      <AppTile title={"Liquidity"} className={"rest"}>
-        <SimpleTable rows={liquidityRows} />
-      </AppTile>
-      <AppTile title={"Allocation History"} className={"allocationHistory"}>
+      <div style={{ display: "none" }}>
+        <h3>Allocation</h3>
         <SimpleTable rows={allocationRows} />
-      </AppTile>
+      </div>
+      <a {...downloadAnchorProps}>Hidden Genesis Data Download Link</a>
     </>
   )
 }
 
 const _dateString = (date: Date) => {
-  const year = date.getFullYear();
-  const month = date.getMonth();
-  const day = date.getDay();
-  const hour = date.getHours();
-  const minutes = date.getMinutes();
-  const seconds = date.getSeconds();
-  return `${year}${month}${day}${hour}${minutes}${seconds}`;
+  const year = date.getFullYear()
+  const month = date.getMonth()
+  const day = date.getDay()
+  const hour = date.getHours()
+  const minutes = date.getMinutes()
+  const seconds = date.getSeconds()
+  return `${year}${month}${day}${hour}${minutes}${seconds}`
 }
 
 export default Genesis
