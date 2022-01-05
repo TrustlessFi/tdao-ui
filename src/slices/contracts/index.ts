5614d9ed1a73293a28326913df50641a8eb6df20
br4e <br4e@protonmail.com>
2022-01-05 14:17:19 -0700

    update packages

--- src/slices/contracts/index.ts
+++ src/slices/contracts/index.ts
@@ -9,30 +9,30 @@ import getContract from "../../utils/getContract"
 
 // TODO add TCP Allocation
 export enum ProtocolContract {
-  Accounting = "Accounting",
-  Auctions = "Auctions",
-  EnforcedDecentralization = "EnforcedDecentralization",
-  Hue = "Hue",
-  HuePositionNFT = "HuePositionNFT",
-  LendHue = "LendHue",
-  Liquidations = "Liquidations",
-  Market = "Market",
-  Prices = "Prices",
-  ProtocolLock = "ProtocolLock",
-  Rates = "Rates",
-  Rewards = "Rewards",
-  Settlement = "Settlement",
-  Tcp = "Tcp",
-  TcpGovernorAlpha = "TcpGovernorAlpha",
-  TcpTimelock = "TcpTimelock",
-  TcpAllocation = "TcpAllocation",
+  Accounting = 'Accounting',
+  Auctions = 'Auctions',
+  EnforcedDecentralization = 'EnforcedDecentralization',
+  Hue = 'Hue',
+  HuePositionNFT = 'HuePositionNFT',
+  LendHue = 'LendHue',
+  Liquidations = 'Liquidations',
+  Market = 'Market',
+  Prices = 'Prices',
+  ProtocolLock = 'ProtocolLock',
+  Rates = 'Rates',
+  Rewards = 'Rewards',
+  Settlement = 'Settlement',
+  Tcp = 'Tcp',
+  TcpGovernorAlpha = 'TcpGovernorAlpha',
+  TcpTimelock = 'TcpTimelock',
+  TcpAllocation = 'TcpAllocation',
 }
 
 export enum RootContract {
-  Governor = "Governor",
-  ProtocolDataAggregator = "ProtocolDataAggregator",
-  GenesisAllocation = "GenesisAllocation",
-  TrustlessMulticall = "TrustlessMulticall",
+  Governor = 'Governor',
+  ProtocolDataAggregator = 'ProtocolDataAggregator',
+  GenesisAllocation = 'GenesisAllocation',
+  TrustlessMulticall = 'TrustlessMulticall',
 }
 
 export enum TDaoContract {
